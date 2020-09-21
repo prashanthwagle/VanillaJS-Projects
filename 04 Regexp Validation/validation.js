779cbfd8f89ef97c48d68128350c2b4e26b9519c
Prashanth <prashanthwagle360@gmail.com>
2020-09-21 10:42:36 +0530

    Completed the Regexp for the form

--- 04 Regexp Validation/validation.js
+++ 04 Regexp Validation/validation.js
@@ -3,6 +3,9 @@ const inputs = document.querySelectorAll("input");
 const patterns = {
   username: /^[a-z\d]{5,12}$/i,
   telephone: /^\d{11}$/,
+  slug: /^[a-z\d-]{8,20}$/,
+  password: /^[\w@-]{8,20}$/,
+  email: /^([\w\.-]+)@([a-z\d-]+)\.([a-z]{2,8})(\.[a-z]{2,8})?$/,
 };
 
 function validate(field, regex) {
